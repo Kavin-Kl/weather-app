47067e96565714e59606eac6282ef6d79fb39d0f
Kavin <kavinkl20@gmail.com>
2025-07-05 22:42:20 +0530

    fn

--- weather_app/lib/core/providers.dart
+++ weather_app/lib/core/providers.dart
@@ -2,6 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dio/dio.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-final dioProvider = Provider<Dio>((ref) => Dio());
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio();
+  dio.options.headers['User-Agent'] =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
+  return dio;
+});
 
 final hiveProvider = Provider<HiveInterface>((ref) => Hive);
